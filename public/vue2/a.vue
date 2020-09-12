c8d29f33494ef76b7510ca104dbab726cc07240b
waha1206 <waha1206@gmail.com>
2020-09-13 05:37:51 +0800

    差點誤刪

---
+++ public/vue2/a.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="tt">
+	{{age}}
+	</div>
+</template>
+
+<script>
+	export default {
+		name = '',
+		data() {
+			return {
+				msg:'Hello World!'
+			}
+		},
+
+	}
+</script>
+
+<style lang="scss" scoped>
+.tt{
+	color: red;
+}
+
+</style>
