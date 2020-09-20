5fe2d9b3748ac2c903124838c3ce6efb63359e1c
waha1206 <waha1206@gmail.com>
2020-09-21 01:27:58 +0800

    移除 css-html套件

--- router.js
+++ router.js
@@ -1,137 +1,146 @@
-var express = require('express')
+var express = require('express');
 
-var md5 = require('blueimp-md5')
+var md5 = require('blueimp-md5');
 
-var User = require('./models/user')
+var User = require('./models/user');
 
 // const session = require('express-session')
 
-var router = express.Router()
+var router = express.Router();
 
-router.get('/', function(req, res, next) {
-    // console.log(req.session.user)
-    console.log(req.session.user)
-    res.render('index.html', {
-        user: req.session.user
-    })
-})
+router.get('/', function (req, res, next) {
+  // console.log(req.session.user)
+  console.log(req.session.user);
+  res.render('index.html', {
+    user: req.session.user,
+  });
+});
 
-router.get('/login', function(req, res, next) {
-    res.render('login.html')
-})
+router.get('/login', function (req, res, next) {
+  res.render('login.html');
+});
 
-router.post('/login', function(req, res, next) {
-    // 獲取表單數據
-    // 查詢數據庫，用戶名與密碼是否正確
-    // 發動響應數據
-    console.log(req.body)
-    var body = req.body
+router.post('/login', function (req, res, next) {
+  // 獲取表單數據
+  // 查詢數據庫，用戶名與密碼是否正確
+  // 發動響應數據
+  console.log(req.body);
+  var body = req.body;
 
-    User.findOne({
-        email: body.email,
-        password: md5(md5(body.password))
-    }, function(err, user) {
+  User.findOne(
+    {
+      email: body.email,
+      password: md5(md5(body.password)),
+    },
+    function (err, user) {
+      if (err) {
+        return next(err);
+      }
+
+      if (!user) {
+        return res.status(200).json({
+          err_code: 1,
+          message: 'email or password is invalid.',
+        });
+      }
+
+      req.session.user = user;
+      res.status(200).json({
+        err_code: 0,
+        message: 'OK',
+      });
+    }
+  );
+});
+
+router.get('/register', function (req, res, next) {
+  res.render('register.html');
+});
+
+router.post('/register', function (req, res, next) {
+  // 1.獲取表單提供的數據 -- 安裝 Express body-parser 中間件
+  // 2.操作據庫
+  // 判斷該用戶是否存在？
+  // 如果已存在，不允許註冊
+  // 如果不存在，註冊新用戶
+  // 3.發送響應
+  console.log(req.body);
+  var body = req.body;
+  User.findOne(
+    {
+      $or: [
+        {
+          email: body.email,
+        },
+        {
+          nickname: body.username,
+        },
+      ],
+    },
+    function (err, data, next) {
+      if (err) {
+        return next(err);
+      }
+      if (data) {
+        console.log(data);
+        // 郵箱或是暱稱已經存在
+        return res.status(200).json({
+          err_code: 1,
+          message: '郵箱或是暱稱已存在',
+        });
+      }
+      // md5 加密兩次防止被破解
+      body.password = md5(md5(body.password));
+      new User(body).save(function (err, user) {
         if (err) {
-            return next(err)
+          return next(err);
         }
-
-        if (!user) {
-            return res.status(200).json({
-                err_code: 1,
-                message: 'email or password is invalid.'
-            })
-        }
-
-        req.session.user = user
+        // 註冊成功，使用 session 紀錄用戶的登錄狀態
+        req.session.user = user;
+        // express 提供了一個方法 json({ key:value }) 會直接發送一個 json 格式給 瀏覽器端
         res.status(200).json({
-            err_code: 0,
-            message: 'OK'
-        })
-    })
-})
+          err_code: 0,
+          message: 'ok',
+        });
+      });
+    }
+  );
+});
 
-router.get('/register', function(req, res, next) {
-    res.render('register.html')
-})
+router.get('/logout', function (req, res, next) {
+  // 清除登錄'狀態
+  req.session.user = null;
+  // 重定向到登錄頁面
+  res.redirect('/login');
+});
 
-router.post('/register', function(req, res, next) {
-    // 1.獲取表單提供的數據 -- 安裝 Express body-parser 中間件
-    // 2.操作據庫
-    // 判斷該用戶是否存在？
-    // 如果已存在，不允許註冊
-    // 如果不存在，註冊新用戶
-    // 3.發送響應
-    console.log(req.body)
-    var body = req.body
-    User.findOne({
-        $or: [{
-                email: body.email
-            },
-            {
-                nickname: body.username
-            }
-        ]
-    }, function(err, data, next) {
-        if (err) {
-            return next(err)
-        }
-        if (data) {
-            console.log(data)
-                // 郵箱或是暱稱已經存在
-            return res.status(200).json({
-                err_code: 1,
-                message: '郵箱或是暱稱已存在'
-            })
-        }
-        // md5 加密兩次防止被破解
-        body.password = md5(md5(body.password))
-        new User(body).save(function(err, user) {
-            if (err) {
-                return next(err)
-            }
-            // 註冊成功，使用 session 紀錄用戶的登錄狀態
-            req.session.user = user
-                // express 提供了一個方法 json({ key:value }) 會直接發送一個 json 格式給 瀏覽器端
-            res.status(200).json({
-                err_code: 0,
-                message: 'ok'
-            })
-        })
-    })
-})
+router.get('/test', function (req, res, next) {
+  console.log(req.body);
+  User.find({}, function (err, user) {
+    if (err) {
+      return next(err);
+    }
+    res.json(user);
+  });
+});
+router.post('/test', function (req, res, next) {
+  console.log(req.body);
 
-router.get('/logout', function(req, res, next) {
-    // 清除登錄'狀態
-    req.session.user = null
-        // 重定向到登錄頁面
-    res.redirect('/login')
-})
+  var body = req.body;
+  console.log(body.nickname);
+  User.findOne(
+    {
+      nickname: body.nickname,
+    },
+    function (err, user) {
+      if (err) {
+        return next(err);
+      }
+      console.log(user);
+      //res.setHeader() 返回什麼數值，就要設定相對的 Header
+      res.json(user);
+    }
+  );
+});
 
-router.get('/test', function(req, res, next) {
-    console.log(req.body)
-    User.find({}, function(err, user) {
-        if (err) {
-            return next(err)
-        }
-        res.json(user)
-    })
-})
-router.post('/test', function(req, res, next) {
-    console.log(req.body)
-
-    var body = req.body
-    console.log(body.nickname)
-    User.findOne({
-        nickname: body.nickname
-    }, function(err, user) {
-        if (err) {
-            return next(err)
-        }
-        console.log(user)
-            //res.setHeader() 返回什麼數值，就要設定相對的 Header
-        res.json(user)
-    })
-})
-
-
-module.exports = router
+module.exports = router;
--- routes/admin.js
+++ routes/admin.js
@@ -1,184 +1,196 @@
-var express = require('express')
+var express = require('express');
 
-var md5 = require('blueimp-md5')
+var md5 = require('blueimp-md5');
 
-var User = require('../models/user')
+var User = require('../models/user');
 
-const session = require('express-session')
+const session = require('express-session');
 
-var admin = express.Router()
+var admin = express.Router();
 
-var User = require('../models/user')
+var User = require('../models/user');
 
 // 使用 await
-var await = require('await')
-var async = require("async");
+var await = require('await');
+var async = require('async');
 // 開啟 csv檔
-var fs = require('fs')
-var parse = require('csv-parse')
-    // 使用 csvtojson
-var csv = require("csvtojson");
-const { send } = require('process')
+var fs = require('fs');
+var parse = require('csv-parse');
+// 使用 csvtojson
+var csv = require('csvtojson');
+const { send } = require('process');
 
-router.get('/admin', function(req, res, next) {
-    var user = req.session.user
-    User.find({},
-        function(err, data) {
-            if (err) {
-                return next(err)
-            }
-            console.log(data)
-            res.render('./settings/user-authority.html', {
-                user: user,
-                users: data
-            })
-        })
+router.get('/admin', function (req, res, next) {
+  var user = req.session.user;
+  User.find({}, function (err, data) {
+    if (err) {
+      return next(err);
+    }
+    console.log(data);
+    res.render('./settings/user-authority.html', {
+      user: user,
+      users: data,
+    });
+  });
 
-    User.find({}, function(err, users, next) {
-        if (err) {
-            return next(err)
-        }
-    })
-})
+  User.find({}, function (err, users, next) {
+    if (err) {
+      return next(err);
+    }
+  });
+});
 
-router.get('/admin/test', function(req, res, next) {
-    var user = req.session.user
-    User.find({},
-        function(err, data) {
-            if (err) {
-                return next(err)
-            }
-            console.log(req.query)
-            console.log(req.url)
+router.get('/admin/test', function (req, res, next) {
+  var user = req.session.user;
+  User.find({}, function (err, data) {
+    if (err) {
+      return next(err);
+    }
+    console.log(req.query);
+    console.log(req.url);
 
-            // 使用 csv-parse 可以工作喔，先暫時註解掉
-            //var csvData = [];
-            // fs.createReadStream('C:/Users/User/Desktop/blog/public/file/customer.csv')
-            // .pipe(parse({ delimiter: ',' }))
-            // .on('data', function(csvrow) {
-            //     console.log(csvrow);
-            //     //do something with csvrow
-            //     csvData.push(csvrow);
-            // })
-            // .on('end', function() {
-            //     //do something with csvData
-            //     console.log(csvData);
-            // });
+    // 使用 csv-parse 可以工作喔，先暫時註解掉
+    //var csvData = [];
+    // fs.createReadStream('C:/Users/User/Desktop/blog/public/file/customer.csv')
+    // .pipe(parse({ delimiter: ',' }))
+    // .on('data', function(csvrow) {
+    //     console.log(csvrow);
+    //     //do something with csvrow
+    //     csvData.push(csvrow);
+    // })
+    // .on('end', function() {
+    //     //do something with csvData
+    //     console.log(csvData);
+    // });
 
-            const csvFilePath = 'C:/Users/User/Desktop/blog/public/file/customer2.csv'
-            const csv = require('csvtojson')
-            csv()
-                .fromFile(csvFilePath)
-                .then((jsonObj) => {
-                    // res.setHeader('Access-Control-Allow-Origin', '*');
-                    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-                    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-                    // res.setHeader('Access-Control-Allow-Credentials', true); // If needed
-                    // send(data)
+    const csvFilePath = 'C:/Users/User/Desktop/blog/public/file/customer2.csv';
+    const csv = require('csvtojson');
+    csv()
+      .fromFile(csvFilePath)
+      .then((jsonObj) => {
+        // res.setHeader('Access-Control-Allow-Origin', '*');
+        // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+        // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+        // res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+        // send(data)
 
-                    console.log(jsonObj[0]);
-                    res.send(jsonObj)
+        console.log(jsonObj[0]);
+        res.send(jsonObj);
 
-                    /**
-                     * [
-                     * 	{a:"1", b:"2", c:"3"},
-                     * 	{a:"4", b:"5". c:"6"}
-                     * ]
-                     */
-                })
+        /**
+         * [
+         * 	{a:"1", b:"2", c:"3"},
+         * 	{a:"4", b:"5". c:"6"}
+         * ]
+         */
+      });
 
-            // Async / await usage
-            //const jsonArray = await (csv().fromFile('C:/Users/User/Desktop/blog/public/file/customer2.csv'))
-            //console.log(jsonArray)
+    // Async / await usage
+    //const jsonArray = await (csv().fromFile('C:/Users/User/Desktop/blog/public/file/customer2.csv'))
+    //console.log(jsonArray)
 
+    //console.log(data)
+    // res.render('./settings/user-authority.html', {
+    //     user: user,
+    //     users: data
+    // })
+    // node cros 跨域請求 的設定
+    return;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+    ); // If needed
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-Requested-With,content-type'
+    ); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+    console.log(jsonArray);
+    res.send(data); //-- 舊的客戶資料
 
-            //console.log(data)
-            // res.render('./settings/user-authority.html', {
-            //     user: user,
-            //     users: data
-            // })
-            // node cros 跨域請求 的設定
-            return
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-            res.setHeader('Access-Control-Allow-Credentials', true); // If needed
-            console.log(jsonArray)
-            res.send(data) //-- 舊的客戶資料
+    //console.log('送出資料')
+  });
+});
 
-            //console.log('送出資料')
+router.get('/admin/test2', function (req, res, next) {
+  var user = req.session.user;
+  console.log('A幹，我是GET耶');
+  User.find({}, function (err, data) {
+    if (err) {
+      return next(err);
+    }
+    console.log(req.query);
+    console.log(req.url);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+    ); // If needed
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-Requested-With,content-type'
+    ); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+    console.log(data);
+    res.send(data); //-- 舊的客戶資料
+    console.log('送出資料');
+  });
+});
 
-        })
-})
+router.post('/admin/test', function (req, res, next) {
+  var user = req.session.user;
+  User.find({}, function (err, data) {
+    if (err) {
+      return next(err);
+    }
+    console.log(req.body);
+    //console.log(data)
+    // res.render('./settings/user-authority.html', {
+    //     user: user,
+    //     users: data
+    // })
+    res.send(data);
+  });
+});
 
-router.get('/admin/test2', function(req, res, next) {
-    var user = req.session.user
-    console.log('A幹，我是GET耶')
-    User.find({},
-        function(err, data) {
-            if (err) {
-                return next(err)
-            }
-            console.log(req.query)
-            console.log(req.url)
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-            res.setHeader('Access-Control-Allow-Credentials', true); // If needed
-            console.log(data)
-            res.send(data) //-- 舊的客戶資料
-            console.log('送出資料')
-        })
-})
+router.delete('', function (req, res, next) {
+  var user = req.session.user;
+  console.log(req.body);
+});
 
-router.post('/admin/test', function(req, res, next) {
-    var user = req.session.user
-    User.find({},
-        function(err, data) {
-            if (err) {
-                return next(err)
-            }
-            console.log(req.body)
-                //console.log(data)
-                // res.render('./settings/user-authority.html', {
-                //     user: user,
-                //     users: data
-                // })
-            res.send(data)
-        })
-})
+router.post('/admin/test2', function (req, res, next) {
+  var user = req.session.user;
+  console.log(req.body);
+  User.find({}, function (err, data) {
+    if (err) {
+      return next(err);
+    }
 
-router.delete('', function(req, res, next) {
-    var user = req.session.user
-    console.log(req.body)
-})
+    console.log('有跑到 delete 的 router 裡面', req.body);
 
-router.post('/admin/test2', function(req, res, next) {
-    var user = req.session.user
-    console.log(req.body)
-    User.find({},
-        function(err, data) {
-            if (err) {
-                return next(err)
-            }
+    //console.log(data)
+    // res.render('./settings/user-authority.html', {
+    //     user: user,
+    //     users: data
+    // })
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+    ); // If needed
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-Requested-With,content-type'
+    ); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+    res.header('Access-Control-Expose-Headers', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Content-Type,Access-Token,adminid'
+    );
+    res.send(data);
+  });
+});
 
-            console.log('有跑到 delete 的 router 裡面', req.body)
-
-            //console.log(data)
-            // res.render('./settings/user-authority.html', {
-            //     user: user,
-            //     users: data
-            // })
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-            res.setHeader('Access-Control-Allow-Credentials', true); // If needed
-            res.header("Access-Control-Expose-Headers", "*")
-            res.header("Access-Control-Allow-Headers", "Content-Type,Access-Token,adminid");
-            res.send(data)
-        })
-})
-
-
-
-module.exports = admin
+module.exports = admin;
